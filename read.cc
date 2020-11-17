d4c1174fe9d210167900f3488dc0a75a0cc0f8bb
Dan Cooke <dcooke@stevens.edu>
2020-11-17 08:32:59 -0500

    updated to take in args

--- read.cc
+++ read.cc
@@ -2,17 +2,20 @@
 #include <fstream>
 #include <iomanip>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::ifstream test("test.out", std::ios::in|std::ios::binary|std::ios::ate);
-    std::streampos size = test.tellg();
+    std::ifstream exe(argv[1], std::ios::in|std::ios::binary|std::ios::ate);
+    std::streampos size = exe.tellg();
     char* binary = new char[size];
-    test.seekg(0, std::ios::beg);
-    test.read(binary, size);
-    test.close();
+    exe.seekg(0, std::ios::beg);
+    exe.read(binary, size);
+    exe.close();
+
     for(int i = 0; i < 32; i++)
     {
         std::cout << std::setfill('0') << std::setw(2) << std::right << std::hex << int(binary[i]) << " ";
     }
+    std::cout << std::endl;
+
     return 0;
 }
