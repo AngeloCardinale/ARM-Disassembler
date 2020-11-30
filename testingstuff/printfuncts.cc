3a2042e3445a8aca164e0d9a2c250fc03fdbd50f
Dan Cooke <dcooke@stevens.edu>
2020-11-30 14:16:39 -0500

    functions for printing start

---
+++ testingstuff/printfuncts.cc
@@ -0,0 +1,71 @@
+#include <fstream>
+
+void dataprocessing()
+{
+
+}
+
+void multiply()
+{
+
+}
+
+void multiplylong()
+{
+
+}
+
+void singledataswap()
+{
+
+}
+
+void branchandex()
+{
+
+}
+
+void halfdatatransreg()
+{
+
+}
+
+void halfdatatransimm()
+{
+
+}
+
+void singledatatrans()
+{
+
+}
+
+void blockdatatrans()
+{
+
+}
+
+void branch()
+{
+
+}
+
+void coprodatatrans()
+{
+
+}
+
+void coprodataop()
+{
+
+}
+
+void coproregtrans()
+{
+
+}
+
+void softwareinterrupt()
+{
+
+}
