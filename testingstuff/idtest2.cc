abf063353f02e2f3ed85c0ba01e81a84db015155
AngeloCardinale <Angelo.a.cardinale@gmail.com>
2020-11-30 14:55:56 -0500

    Fixed up idtest2

--- testingstuff/idtest2.cc
+++ testingstuff/idtest2.cc
@@ -18,10 +18,13 @@ int main()
                 if((temp & 0x0FF00F) == 0x010000 ||(temp & 0x0FF00F)== 0x014000)
                 {
                     std::cout << "single data swap\n";
+                    return 0;
                 }
+
                 else if((temp & 0x0E400F) == 0x000000)
                 {
                     std::cout << "Halfword data transfer register offset\n";
+                    return 0;
                 }
 
                 temp = temp >> 12;
@@ -29,90 +32,124 @@ int main()
                 if((temp & 0x0F8) == 0x008)
                 {
                     std::cout << "multiply long\n";
+                    return 0;
                 }
+
                 else if((temp & 0x0FC) == 0x000)
                 {
                     std::cout << "multiply \n";
+                    return 0;
                 }
                 
         }
+
         else if((temp & 0x012FFF1) == 0x012FFF1)                        //Check if Branch and Exchange
         {
             std::cout << "branch and exchange\n";
+            return 0;
         }
+
         else if((temp & 0x0E40009) == 0x0E40009)
         {
             std::cout << "Halfword data transfer immediate offset\n";
+            return 0;
         }
+
         else if((temp & 0x0E40009) == 0x0E00009)
         {
             std::cout << "Halfword data transfer register offset\n";
+            return 0;
         }
 
         temp = temp >> 20;
 
         if((temp & 0x0F) == 0x0F)                                  //27th to 24th bits are 1111
         {
-            std::cout << "Software interrupt\n";                
+            std::cout << "Software interrupt\n";    
+            return 0;            
         }   
 
         else if((temp & 0x0E) == 0x0E)                                  //27th to 24th bits are 1110
         {
-            std::cout << "Coprocessor register transfer\n";                
+            std::cout << "Coprocessor register transfer\n";  
+            return 0;              
         }  
 
         else if((temp & 0x0C) == 0x00)
         {
             std::cout << "Data processing\n";
-        }          
+            return 0;
+        }         
+
         else if((temp & 0x0E) == 0x0C)  
         {
             std::cout << "Coprocessor data transfer\n";
+            return 0;
         } 
+
          else if((temp & 0x0E) == 0x0A)  
         {
             std::cout << "branch\n";
-        }     
+            return 0;
+        }    
+
          else if((temp & 0x0E) == 0x08)  
         {
             std::cout << "Block data transfer\n";
+            return 0;
         }   
+
         else if((temp & 0x0C) == 0x04)  
         {
             std::cout << "Single data transfer\n";
+            return 0;
         }                                                         
         
     }
     else                                                                //first bit is 0
     {
         temp = temp >> 20;
+
         if((temp & 0x0E) == 0x0E)                                       //27th to 24th bits are 1110
         {
-            std::cout << "Coprocessor data operation\n";                
+            std::cout << "Coprocessor data operation\n";  
+            return 0;              
         }
+
         else if((temp & 0x0F) == 0x0F)                                  //27th to 24th bits are 1111
         {
-            std::cout << "Software interrupt\n";                
-        }                    
+            std::cout << "Software interrupt\n";  
+            return 0;              
+        }    
+
         else if((temp & 0x0C) == 0x00)
         {
             std::cout << "Data processing\n";
-        }          
+            return 0;
+        }       
+
         else if((temp & 0x0E) == 0x0C)  
         {
             std::cout << "Coprocessor data transfer\n";
+            return 0;
         } 
+
          else if((temp & 0x0E) == 0x0A)  
         {
             std::cout << "branch\n";
-        }     
+            return 0;
+        }    
+
          else if((temp & 0x0E) == 0x08)  
         {
             std::cout << "Block data transfer\n";
+            return 0;
         }   
+
         else if((temp & 0x0C) == 0x04)  
         {
             std::cout << "Single data transfer\n";
+            return 0;
         }   
     }
 }
