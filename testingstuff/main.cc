fe944b445c44f574cca811a83aeba7291e8e4719
Dan Cooke <dcooke@stevens.edu>
2020-12-09 09:40:12 -0500

    added multiply

--- testingstuff/main.cc
+++ testingstuff/main.cc
@@ -12,13 +12,6 @@ using namespace std;
 
 int main()
 {
-    int op1 = 0x92001510; //data processing
-    int op2 = 0x0200A0E3; //data processing
-    int op3 = 0x30237190; //muti
-    int op4 = 0x30837190; //mutiply long
-    int op5 = 0x310F4090; //Single data swap
-    int op6 = 0x3E0F4080; //Coprocessor data operation
-
     vector<uint32_t> instructions = readexe("test.out");
     int i = 61;
     cout << hex << instructions.at(i * 4) << endl;
--- testingstuff/printfuncts.cc
+++ testingstuff/printfuncts.cc
@@ -6,7 +6,7 @@ using namespace std;
 void branchandex(uint32_t instruction)
 {
     uint32_t cond = instruction >> 28;
-    uint32_t Rn = instruction & 0x0000000F;
+    uint32_t Rn = instruction & 0xF;
     cout << dec << "BX" << condition(cond) << " " << reg(Rn) << "\n";
 }
 
@@ -17,7 +17,35 @@ void dataprocessing(uint32_t instruction)
 
 void multiply(uint32_t instruction)
 {
-    cout << "multiply\n";
+    uint32_t cond = instruction >> 28;
+    uint32_t A = (instruction >> 21) & 0x1;
+    uint32_t S = (instruction >> 20) & 0x1;
+    uint32_t Rd = (instruction >> 16) & 0xF;
+    uint32_t Rn = (instruction >> 12) & 0xF;
+    uint32_t Rs = (instruction >> 8) & 0xF;
+    uint32_t Rm = instruction & 0xF;
+    if(A == 1)
+    {
+        if(S == 1)
+        {
+            cout << dec << "MLA" << condition(cond) << "S " << reg(Rd) << "," << reg(Rm) << "," << reg(Rs) << "," << reg(Rn) << "\n";
+        }
+        else
+        {
+            cout << dec << "MLA" << condition(cond) << " " << reg(Rd) << "," << reg(Rm) << "," << reg(Rs) << "," << reg(Rn) << "\n";
+        }
+    }
+    else
+    {
+        if(S == 1)
+        {
+            cout << dec << "MUL" << condition(cond) << "S " << reg(Rd) << "," << reg(Rm) << "," << reg(Rs) << "\n";
+        }
+        else
+        {
+            cout << dec << "MUL" << condition(cond) << " " << reg(Rd) << "," << reg(Rm) << "," << reg(Rs) << "\n";
+        }
+    }
 }
 
 void multiplylong(uint32_t instruction)
