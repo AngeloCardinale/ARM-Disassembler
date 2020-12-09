75cd83c6482cc866ddaacbe1c3fb6cd42246bc4c
Dan Cooke <dcooke@stevens.edu>
2020-12-09 10:28:52 -0500

    added multi long and singledataswap

--- testingstuff/printfuncts.cc
+++ testingstuff/printfuncts.cc
@@ -10,11 +10,6 @@ void branchandex(uint32_t instruction)
     cout << dec << "BX" << condition(cond) << " " << reg(Rn) << "\n";
 }
 
-void dataprocessing(uint32_t instruction)
-{
-    cout << "data processing\n";
-}
-
 void multiply(uint32_t instruction)
 {
     uint32_t cond = instruction >> 28;
@@ -50,12 +45,81 @@ void multiply(uint32_t instruction)
 
 void multiplylong(uint32_t instruction)
 {
-    cout << "multiply long\n";
+    uint32_t cond = instruction >> 28;
+    uint32_t U = (instruction >> 22) & 0x1;
+    uint32_t A = (instruction >> 21) & 0x1;
+    uint32_t S = (instruction >> 20) & 0x1;
+    uint32_t RdHi = (instruction >> 16) & 0xF;
+    uint32_t RdLo = (instruction >> 12) & 0xF;
+    uint32_t Rs = (instruction >> 8) & 0xF;
+    uint32_t Rm = instruction & 0xF;
+    if(U == 1)
+    {
+        if(A == 1)
+        {
+            if(S == 1)
+            {
+                cout << dec << "SMLAL" << condition(cond) << "S " << reg(RdLo) << "," << reg(RdHi) << "," << reg(Rm) << "," << reg(Rs) << "\n";
+            }
+            else
+            {
+                cout << dec << "SMLAL" << condition(cond) << " " << reg(RdLo) << "," << reg(RdHi) << "," << reg(Rm) << "," << reg(Rs) << "\n";
+            }
+        }
+        else
+        {
+            if(S == 1)
+            {
+                cout << dec << "SMULL" << condition(cond) << "S " << reg(RdLo) << "," << reg(RdHi) << "," << reg(Rm) << "," << reg(Rs) << "\n";
+            }
+            else
+            {
+                cout << dec << "SMULL" << condition(cond) << " " << reg(RdLo) << "," << reg(RdHi) << "," << reg(Rm) << "," << reg(Rs) << "\n";
+            }
+        }
+    }
+    else
+    {
+        if(A == 1)
+        {
+            if(S == 1)
+            {
+                cout << dec << "UMLAL" << condition(cond) << "S " << reg(RdLo) << "," << reg(RdHi) << "," << reg(Rm) << "," << reg(Rs) << "\n";
+            }
+            else
+            {
+                cout << dec << "UMLAL" << condition(cond) << " " << reg(RdLo) << "," << reg(RdHi) << "," << reg(Rm) << "," << reg(Rs) << "\n";
+            }
+        }
+        else
+        {
+            if(S == 1)
+            {
+                cout << dec << "UMULL" << condition(cond) << "S " << reg(RdLo) << "," << reg(RdHi) << "," << reg(Rm) << "," << reg(Rs) << "\n";
+            }
+            else
+            {
+                cout << dec << "UMULL" << condition(cond) << " " << reg(RdLo) << "," << reg(RdHi) << "," << reg(Rm) << "," << reg(Rs) << "\n";
+            }
+        }
+    }
 }
 
 void singledataswap(uint32_t instruction)
 {
-    cout << "single data swap\n";
+    uint32_t cond = instruction >> 28;
+    uint32_t B = (instruction >> 22) & 0x1;
+    uint32_t Rn = (instruction >> 16) & 0x1;
+    uint32_t Rd = (instruction >> 12) & 0x1;
+    uint32_t Rm = instruction & 0xF;
+    if(B == 1)
+    {
+        cout << dec << "SWP" << condition(cond) << "B " << reg(Rd) << "," << reg(Rm) << ",[" << reg(Rn) << "]\n";
+    }
+    else
+    {
+        cout << dec << "SWP" << condition(cond) << " " << reg(Rd) << "," << reg(Rm) << ",[" << reg(Rn) << "]\n";
+    }
 }
 
 void halfdatatransreg(uint32_t instruction)
@@ -102,4 +166,9 @@ void softwareinterrupt(uint32_t instruction)
 {
     uint32_t cond = instruction >> 28;
     cout << dec << "SWI" << condition(cond) << "\n"; //inc for now
+}
+
+void dataprocessing(uint32_t instruction)
+{
+    cout << "data processing\n";
 }
