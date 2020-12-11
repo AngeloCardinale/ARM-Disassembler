8832029e569f174048628776b13100604884b8bb
Michael DiGregorio <mjm.digregorio@gmail.com>
2020-12-11 14:00:20 -0500

    Update instruction_types.cc
    removed spurious map, we dont need the extra expected values

--- src/instruction_types.cc
+++ src/instruction_types.cc
@@ -40,27 +40,8 @@ std::map<uint32_t, instruction_type> bitmasks({
     // {hex_for_mask_to_check_type, instruction_type}
 });
 
-std::unordered_map<instruction_type, uint32_t> expected_values({
-    {data_processing,       0x000001},
-    {multiply,              0x000002},
-    {multiply_long,         0x000003},
-    {single_data_swap,      0x000004},
-    {branch_and_exchange,   0x000005},
-    {hwdt_reg_offset,       0x000006},
-    {hwdt_imm_offset,       0x000007},
-    {single_data_transfer,  0x000008},
-    {block_data_transfer,   0x000009},
-    {branch,                0x00000A},
-    {co_data_transfer,      0x00000B},
-    {co_data_operation,     0x00000C},
-    {co_reg_transfer,       0x00000D},
-    {software_interrupt,    0x00000E},
-    // this will be for the expected value for check bits if its different than the mask
-});
-
 instruction_type get_instruction_type(uint32_t instruction) {
     for (const auto& [mask, type] : bitmasks) {
-        uint32_t expected = expected_values[type];
         if (check_bits(instruction, mask, mask) == 1) {
             return type;
         }
