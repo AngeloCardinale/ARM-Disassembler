51c1e02489fb025986963ffd1139beb1de90e64a
Mycicle <mjm.digregorio@gmail.com>
2020-12-16 13:37:18 -0500

    fixed instruction type bug, success!
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

--- src/instruction_types.cc
+++ src/instruction_types.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 #include <map>
-
+#include <bitset>
 #include "utils.cc"
 
 enum instruction_type {
@@ -22,29 +22,32 @@ enum instruction_type {
     software_interrupt,
 };
 
-std::map<uint32_t, instruction_type> bitmasks({
-    {0x0F000000U, software_interrupt},
-    {0x0E000010U, co_reg_transfer},
-    {0x0E000000U, co_data_operation},
-    {0x0C000000U, co_data_transfer},
-    {0x0A000000U, branch},
-    {0x08000000U, block_data_transfer},
-    {0x04000000U, single_data_transfer},
-    {0x00400090U, hwdt_imm_offset},
-    {0x00000090U, hwdt_reg_offset},
-    {0x012FFF10U, branch_and_exchange},
-    {0x01000090U, single_data_swap},
-    {0x00800090U, multiply_long},
-    {0x00000090U, multiply},
-    {0x00000000U, data_processing},
+std::map<uint32_t, instruction_type> bitmasks ({
+
+    {0x0F000000U, instruction_type::software_interrupt},
+    {0x0E000010U, instruction_type::co_reg_transfer},
+    {0x0E000000U, instruction_type::co_data_operation},
+    {0x0C000000U, instruction_type::co_data_transfer},
+    {0x0A000000U, instruction_type::branch},
+    {0x08000000U, instruction_type::block_data_transfer},
+    {0x04000000U, instruction_type::single_data_transfer},
+    {0x00400090U, instruction_type::hwdt_imm_offset},
+    {0x00000090U, instruction_type::hwdt_reg_offset},
+    {0x012FFF10U, instruction_type::branch_and_exchange},
+    {0x01000090U, instruction_type::single_data_swap},
+    {0x00800090U, instruction_type::multiply_long},
+    {0x00000090U, instruction_type::multiply},
+    {0x00000000U, instruction_type::data_processing},
 
     // {hex_for_mask_to_check_type, instruction_type}
 });
 
 instruction_type get_instruction_type(uint32_t instruction) {
-    for (const auto& [mask, type] : bitmasks) {
-        if (check_bits(instruction, mask, mask) == 1) {
-            if (type == instruction_type::multiply || type == instruction_type::hwdt_reg_offset) {
+    int counter = 0;
+    std::map<uint32_t, instruction_type>::reverse_iterator it;
+    for (it = bitmasks.rbegin(); it != bitmasks.rend(); it++) {
+        if (check_bits(instruction, it->first, it->first) == 1) {
+            if (it->second == instruction_type::multiply || it->second == instruction_type::hwdt_reg_offset) {
                 if(((instruction >> 5) & 0x1U == 0x0U) && ((instruction >> 6) & 0x1U == 0x0U) && ((instruction >> 23) & 0x1U == 0x0U) && ((instruction >> 24) & 0x1U == 0x0U)) {
                     return multiply;
                 }
@@ -52,7 +55,7 @@ instruction_type get_instruction_type(uint32_t instruction) {
                     return hwdt_reg_offset;
                 }
             }
-            return type;
+            return it->second;
         }
     }
 
